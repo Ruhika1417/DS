cc01c38db67bd5388db795e0cc5631bbbfc069d5
Reshuv <71873882+Reshuv@users.noreply.github.com>
2023-05-23 00:22:43 +0530

    ServerInterface.java

--- 2.RMI/ServerInterface.java
+++ 2.RMI/ServerInterface.java
@@ -1,5 +1,6 @@
 import java.rmi.*;
-
+//going to use it remotely, so extend remote
 public interface ServerInterface extends Remote {
     String concat(String a, String b) throws RemoteException;
+    //concat method which is overriden in Servant class
 }
