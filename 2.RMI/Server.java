89df4daac96f5f48be6b2f08667cd0b8d5a47f1e
Reshuv <71873882+Reshuv@users.noreply.github.com>
2023-05-23 00:29:36 +0530

    Server.java

--- 2.RMI/Server.java
+++ 2.RMI/Server.java
@@ -4,7 +4,10 @@ import java.net.*;
 public class Server {
     public static void main(String[] args) {
         try {
+            //create an object of implementor class
             Servant s = new Servant();
+            //rmiregistry contains concat object , to indentify the it use nameing for that need to bind server name with object
+            //rebind->to bind again if already binded
             Naming.rebind("Server", s);
         } catch (Exception e) {
             System.out.println(e);
